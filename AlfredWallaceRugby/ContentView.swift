0ad33489a501b6adaebf755feb234279a1fcad61
Alfred Wallace <fake.email@gitmail.com>
2022-11-06 20:12:32 +0100

    Adding some global todos

--- AlfredWallaceRugby/ContentView.swift
+++ AlfredWallaceRugby/ContentView.swift
@@ -4,6 +4,15 @@
 //
 //  Created by Arthur Falque Pierrotin on 06/11/2022.
 //
+// https://cmsapi.pulselive.com/rugby/rankings/mru
+//
+// todo 1 : récupérer les data + cas d'erreurs
+// todo 2 : faire un fichier class+struct pour decoder le JSON en Swift data
+// todo 3 : faire une list dans la vue principale
+// todo 4 : faire un bouton + sheet pour ajouter un match
+// todo 5 : que l'on puisse aller et venir entre l'état de départ et l'état post-match
+
+
 
 import SwiftUI
 
