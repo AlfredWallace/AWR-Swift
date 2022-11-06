4af9f71817e94806530813471d771a1d539c484b
Alfred Wallace <fake.email@gitmail.com>
2022-11-06 19:43:29 +0100

    Initial Commit

---
+++ AlfredWallaceRugby/AlfredWallaceRugbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlfredWallaceRugbyApp.swift
+//  AlfredWallaceRugby
+//
+//  Created by Arthur Falque Pierrotin on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlfredWallaceRugbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
